4bcca4778fac82b5a45a4bb308e7fdae4adfb1e0
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-02 16:07:48 +0500

    dart2

--- bin/if02.dart
+++ bin/if02.dart
@@ -8,5 +8,22 @@ Args:
 Returns:
     int: return answer.
 */
+int func(
+  int a,
+  int b,
+  int c,
+) {
+  int x = 0;
+  if (a < b && b < c && a < c) {
+    return a;
+  }
+  if (a > b && b > c && c > a) {
+    return c;
+  }
+  if (a < b && b > c && a > c) ;
+  return c;
+}
+
 void main() {
+  print(func(2, 45, 67));
 }

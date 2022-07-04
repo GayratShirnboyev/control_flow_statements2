e002dc4e45bf545c35d83da3b04a51e4dfd4b2d8
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-04 19:40:48 +0500

    main5

--- bin/if05.dart
+++ bin/if05.dart
@@ -8,9 +8,9 @@ Returns:
 */
 int func(s) {
   int a = s % 10;
-  int b = s ~/ 100 % 10;
+  int b = s ~/ 10 % 10;
   int d = s ~/ 10 % 10 ~/ 10;
-  int k = s % 10 ~/ 100 ~/ 10;
+  int k = s ~/ 10 ~/ 100 % 10;
   int t = s ~/ 10 ~/ 100 ~/ 10;
   if (a > b && a > d && a > k && a > t) {
     return a;
@@ -21,15 +21,16 @@ int func(s) {
   if (d > a && d > b && k < d && d > t) {
     return d;
   }
-  if (k > a && k > b && d < k && k > t) {
+  if (k > a && k > b && k > d && k > t) {
     return k;
   }
   if (t > a && t > b && t > d && t > k) {
     return t;
   }
+
   return 0;
 }
 
 void main() {
-  print(func(84339));
+  print(func(89206));
 }

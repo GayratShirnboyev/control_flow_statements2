4ba561eee0da63f471ccfe2d0d0f4fd62644b9d1
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-04 19:50:19 +0500

    main6

--- bin/if06.dart
+++ bin/if06.dart
@@ -6,5 +6,24 @@ Args:
 Returns:
     int: return answer. 
 */
+int func(a) {
+  int s = a % 10;
+  int v = a ~/ 10 % 10;
+  int d = a ~/ 10 % 100 ~/ 10;
+  int w = a ~/ 10 ~/ 100 % 10;
+  int r = a ~/ 10 ~/ 100 ~/ 10;
+  if (a > v && a > d && a > w && a > r) {
+    return 1;
+  } else if (v > s && v > d && v > w && v > r) {
+    return 2;
+  } else if (d > w && d > r && d > v && d > s) {
+    return 3;
+  } else if (w > r && w > d && w > v && w > s) {
+    return 4;
+  }
+  return 5;
+}
+
 void main() {
+  print(func(13580));
 }

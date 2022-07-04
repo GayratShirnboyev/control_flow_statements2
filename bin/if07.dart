679e79d3d6945e407f9ea0a0907547c81340f2ed
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-04 16:32:28 +0500

    main7

--- bin/if07.dart
+++ bin/if07.dart
@@ -15,5 +15,70 @@ Returns:
     str: return answer. 
 */
 
+// String fssunc(int s) {
+//   Stirng a = 'Monday';
+//   String b = 'tuesday';
+//   String c = 'Wednesday';
+//   String d = 'Thursdey';
+//   String e = 'Friday';
+//   String f = 'Saturday';
+//   String g = 'Sunday';
+// }
+
+// void main() {
+//   print(func(b));
+// }
+String func(
+  int a,
+) {
+  String day = "";
+  if (a == 1) {
+    day = 'Monday';
+    return day;
+  } else if (a == 2) {
+    day = 'Tuesday';
+    return day;
+  } else if (a == 3) {
+    day = 'Wednesday';
+    return day;
+  } else if (a == 4) {
+    day = 'Thursdey';
+    return day;
+  } else if (a == 5) {
+    day = 'Friday';
+    return day;
+  } else if (a == 6) {
+    day = 'Saturday';
+    return day;
+  } else if (a == 7) {
+    day = 'Sunday';
+    return day;
+  }
+  return day;
+}
+// if (a == 2) {
+//   return day;
+// }
+// if (a == 3) {
+//   return day;
+// }
+// if (a == 3) {
+//   return day;
+// }
+// if (a == 4) {
+//   return day;
+// }
+// if (a == 5) {
+//   return day;
+// }
+// if (a == 6) {
+//   return day;
+// }
+// if (a == 7) {
+//   return day;
+// }
+// return d;
+
 void main() {
+  print(func(3));
 }
